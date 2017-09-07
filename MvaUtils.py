1ba402b841bd4e11fdf87201270d8c9961a08f1b
ErickEduardo <isc.ErickAguilar@gmail.com>
2017-09-07 09:51:50 -0500

    cambio en grafica biplot

--- MvaUtils.py
+++ MvaUtils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.stats import chi2
 from matplotlib.patches import Ellipse
 from pyspark.mllib.linalg.distributed import RowMatrix
@@ -31,16 +32,16 @@ def getProbabilityDensityContour(plt,datasetDF,cols,alpha,freedomDegrees,color,n
     mu, sigma = computeMaximumLikelihoodEstimators(datasetDF.select(cols))
     eigenValues, eigenVectors = diagonalize(sigma)
     chiSquaredCriticalVale = chi2.ppf(q=(1-alpha),df=freedomDegrees)
-    print('\t\tResume for '+name+'\nMaximum likehood estimators')
-    print('-Mean vector (Mu):\n'+str(mu))
-    print('-Covariance matrix (Sigma):\n'+str(sigma))
-    print('\nSpectral decomposition')
-    print('-EigenValues of Sigma:\n'+str(eigenValues))
-    print('-EigenVectors matrix of Sigma (Gamma):\n'+str(eigenVectors))
-    print('\nProbability quantile')
-    print('-Chi-Squared critical value at 1-'+str(alpha)\
-          +' percent: '+str(chiSquaredCriticalVale)+'\n\n')
     plotConfidenceEllipse(plt,mu,eigenValues,eigenVectors,chiSquaredCriticalVale,color=color,alpha=0.25)
+    sumaryTable=[]
+    sumaryTable.append(['Mean',mu])
+    sumaryTable.append(['Covariance matrix',sigma])
+    sumaryTable.append(['EigenValues',eigenValues])
+    sumaryTable.append(['EigenVectors',eigenVectors])
+    sumaryTable.append(['Confidence',1-alpha])
+    sumaryTable.append(['Chi-squared critical value',chiSquaredCriticalVale])
+    sumaryTableDF = pd.DataFrame(sumaryTable,columns=['Summary',name])
+    return sumaryTableDF
     
 def scatterPlot(plt,dataDF,col_1,col_2,color):
     dataPD = dataDF.select([col_1,col_2]).toPandas()
@@ -57,11 +58,11 @@ def getCanonicalBase(dim):
 def transformCanonicalBase(plt,components,canonicalBaseDF,colums):
     M = 3*canonicalBaseDF.select(components).toPandas().as_matrix()
     rows,cols = M.T.shape
+    maxes = 1.1*np.amax(abs(M), axis = 0)
     for i,l in enumerate(range(0,cols)):
-        xs = [0,M[i,0]]
-        ys = [0,M[i,1]]
-        plt.plot(xs,ys)
-    plt.plot(0,0,'ok')
-    plt.axis('equal')
-    plt.legend(colums)
-    plt.grid(b=True, which='major')
+        plt.axes().arrow(0,0,M[i,0],M[i,1],head_width=0.05,head_length=0.1,color = 'black')
+    plt.plot(0,0,'ok') #<-- plot a black point at the origin
+    plt.axis('equal')  #<-- set the axes to the same scale
+    plt.xlim([-maxes[0],maxes[0]]) #<-- set the x axis limits
+    plt.ylim([-maxes[1],maxes[1]]) #<-- set the y axis limits
+    plt.grid(b=True, which='major') #<-- plot grid lines
